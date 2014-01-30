89fc2271875427d1bd2a614f32bc96a4472ea0fe
Pascal BARREAU <pbarreau@ac-guadeloupe.fr>
2014-01-29 22:58:10 -0300

    Selection mono cellule ok. Code a nettoyer pour lisibilite !

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,8 @@
 #include <QTextStream>
 #include <QLineEdit>
 #include <QFormLayout>
+#include <QLabel>
+#include <QStandardItemModel>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -22,7 +24,7 @@ MainWindow::MainWindow(QWidget *parent,NE_FDJ::E_typeJeux leJeu, bool load) :
 {
     GererBase DB_tirages;
     tirages tmp(leJeu);
-     QString ficSource;
+    QString ficSource;
 
     ui->setupUi(this);
 
@@ -32,17 +34,45 @@ MainWindow::MainWindow(QWidget *parent,NE_FDJ::E_typeJeux leJeu, bool load) :
 
     QWidget *AfficherBase = new QWidget;
     QTableView *PourLaBase = new QTableView;
-
-
-    //QLineEdit *nom = new QLineEdit;
     QFormLayout *layout = new QFormLayout;
     layout->addWidget(PourLaBase);
     AfficherBase->setLayout(layout);
-
+    AfficherBase->setWindowTitle("Base des tirages");
+    PourLaBase->setSelectionMode(QAbstractItemView::SingleSelection);
+    PourLaBase->setStyleSheet("QTableView {selection-background-color: red;}");
+    PourLaBase->setEditTriggers(QAbstractItemView::NoEditTriggers);
     QMdiSubWindow *sousFenetre1 = zoneCentrale->addSubWindow(AfficherBase);
 
 
-    AfficherBase->setWindowTitle("Base des tirages");
+    QStandardItemModel *modele = new QStandardItemModel(50,2);
+    modele->setHeaderData(0,Qt::Horizontal,"Boules");
+    modele->setHeaderData(1,Qt::Horizontal,"Voisin");
+    //QAbstractItemView::NoEditTriggers
+
+    QStandardItem *item = new QStandardItem( QString::number(222));
+    modele->setItem(10,0,item);
+    QStandardItem *item2 = new QStandardItem( QString::number(240));
+    modele->setItem(8,0,item2);
+
+    QWidget *qwVoisin = new QWidget;
+    QTableView *tblVoisin = new QTableView;
+    tblVoisin->setModel(modele);
+    tblVoisin->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    tblVoisin->sortByColumn(1);
+    QLabel *nbSortie = new QLabel;
+    QFormLayout *layVoisin = new QFormLayout;
+    tblVoisin->setColumnWidth(0,60);
+    tblVoisin->setColumnWidth(1,60);
+    tblVoisin->setMaximumWidth(180);
+    tblVoisin->setMinimumHeight(367);
+    qwVoisin->setMinimumHeight(367);
+    nbSortie->setText("Nb total de sorties:");
+    layVoisin->addWidget(nbSortie);
+    layVoisin->addWidget(tblVoisin);
+    qwVoisin->setLayout(layVoisin);
+    qwVoisin->setWindowTitle("Voisins de selection");
+    QMdiSubWindow *sousFenetre2 = zoneCentrale->addSubWindow(qwVoisin);
+
     setCentralWidget(zoneCentrale);
 
 
