fe017cd9927cf016a39b6a213cb18f832bfd845c
Pascal BARREAU <pbarreau@ac-guadeloupe.fr>
2014-02-07 07:15:26 -0300

    sauvegarde vendredi matin

--- gererbase.cpp
+++ gererbase.cpp
@@ -9,7 +9,7 @@
 #include "tirages.h"
 
 GererBase::GererBase(QObject *parent) :
-    QObject(parent)
+  QObject(parent)
 {
 }
 
@@ -20,91 +20,101 @@ GererBase::~GererBase(void)
 
 bool GererBase::CreerBaseEnMemoire(bool action)
 {
-    // http://developer.nokia.com/community/wiki/Creating_an_SQLite_database_in_Qt
-    db = QSqlDatabase::addDatabase("QSQLITE");
+  // http://developer.nokia.com/community/wiki/Creating_an_SQLite_database_in_Qt
+  db = QSqlDatabase::addDatabase("QSQLITE");
 
-    lieu = action;
-    if(action == true){
+  lieu = action;
+  if(action == true){
 
-        db.setDatabaseName(":memory:");
+      db.setDatabaseName(":memory:");
     }
-    else
+  else
     {
 #ifdef Q_OS_LINUX
-        // NOTE: We have to store database file into user home folder in Linux
-        QString path(QDir::home().path());
-        path.append(QDir::separator()).append("mabase.sqlite");
-        path = QDir::toNativeSeparators(path);
-        db.setDatabaseName(path);
+      // NOTE: We have to store database file into user home folder in Linux
+      QString path(QDir::home().path());
+      path.append(QDir::separator()).append("mabase.sqlite");
+      path = QDir::toNativeSeparators(path);
+      db.setDatabaseName(path);
 #else
-        // NOTE: File exists in the application private folder
-        db.setDatabaseName("mabase.sqlite");
+      // NOTE: File exists in the application private folder
+      db.setDatabaseName("mabase.sqlite");
 #endif
 
     }
 
-    // Open databasee
-    return db.open();
+  // Open databasee
+  return db.open();
 }
 
 QSqlError GererBase::lastError()
 {
-    // If opening database has failed user can ask
-    // error description by QSqlError::text()
-    return db.lastError();
+  // If opening database has failed user can ask
+  // error description by QSqlError::text()
+  return db.lastError();
 }
 
 bool GererBase::SupprimerBase()
 {
-    // Close database
-    db.close();
+  // Close database
+  db.close();
 
 #ifdef Q_OS_LINUX
-    // NOTE: We have to store database file into user home folder in Linux
-    QString path(QDir::home().path());
-    path.append(QDir::separator()).append("mabase.sqlite");
-    path = QDir::toNativeSeparators(path);
-    return QFile::remove(path);
+  // NOTE: We have to store database file into user home folder in Linux
+  QString path(QDir::home().path());
+  path.append(QDir::separator()).append("mabase.sqlite");
+  path = QDir::toNativeSeparators(path);
+  return QFile::remove(path);
 #else
 
-    // Remove created database binary file
-    return QFile::remove("mabase.sqlite");
+  // Remove created database binary file
+  return QFile::remove("mabase.sqlite");
 #endif
 }
 
 void GererBase::AfficherBase(QWidget *parent, QTableView *cibleview)
 {
-    int i;
-    tbl_model = new QSqlTableModel(parent, db);
-    tbl_model->setTable("tirages");
-    tbl_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
-    tbl_model->select();
+  int i=0,j=0, depart = 0;
+  tirages tmp;
+  stTiragesDef def;
 
-    tbl_model->removeColumn(0); // don't show the ID
+  tmp.getConfig(&def);
+
+  tbl_model = new QSqlTableModel(parent, db);
+  tbl_model->setTable("tirages");
+  tbl_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+  tbl_model->select();
+
+  tbl_model->removeColumn(0); // don't show the ID
 
 
+  // Attach it to the view
+  cibleview->setModel(tbl_model);
+  //qDebug() << cibleview->columnWidth(3);
 
-#if 0
-    tbl_model->setHeaderData(0, Qt::Horizontal, tr("b1"));
-    tbl_model->setHeaderData(1, Qt::Horizontal, tr("b2"));
-    tbl_model->setHeaderData(2, Qt::Horizontal, tr("b3"));
-    tbl_model->setHeaderData(3, Qt::Horizontal, tr("b4"));
-    tbl_model->setHeaderData(4, Qt::Horizontal, tr("b5"));
-    tbl_model->setHeaderData(5, Qt::Horizontal, tr("e1"));
-    tbl_model->setHeaderData(6, Qt::Horizontal, tr("e2"));
-#endif
-    // Attach it to the view
-    cibleview->setModel(tbl_model);
-    qDebug() << cibleview->columnWidth(3);
-    for (i=1;i<=7;i++){
-    cibleview->setColumnWidth(i,30);
+    for(i=0; i<def.nb_zone;i++)
+    {
+      if(i)
+        {
+        depart = depart + def.nbElmZone[i-1];
+        }
+      for(j=depart+1;j<depart+def.nbElmZone[i]+1;j++)
+        {
+         cibleview->setColumnWidth(j,30);
+        }
     }
-    for(i=8;i<=11;i++)
+#if 0
+  for (i=1;i<=7;i++){
+      cibleview->setColumnWidth(i,30);
+    }
+#endif
+
+  for(i=j;i<=(tbl_model->columnCount());i++)
     {
       cibleview->hideColumn(i);
     }
 
-    cibleview->setMinimumHeight(390);
-    parent->setMinimumWidth(400);
-    parent->setMinimumHeight(400);
+  cibleview->setMinimumHeight(390);
+  parent->setMinimumWidth(400);
+  parent->setMinimumHeight(400);
 }
