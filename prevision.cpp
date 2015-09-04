a786d0fb85666e1f00857afbc65032ee693926e6
Pascal BARREAU <4.barreau.pascal@gmail.com>
2015-09-04 15:05:31 -0500

    Ecriture nom gerique de colonne dans base (retour a avant rajout boule dans nom colonne)

--- prevision.cpp
+++ prevision.cpp
@@ -9,15 +9,15 @@
 -- comptage boule sur ligne v1
 select tbleft.blgn as B,count(tbleft.blgn) as T, MAX(tbleft.sortie) as Smax from
 (
-select id_6 as blgn, nb_6 as sortie  from BNEXT_b_D1 where id = 1
+select b1k as blgn, b1r as sortie  from BNEXT_b_D1 where id = 1
 union all
-select id_9 as blgn, nb_9 as sortie  from BNEXT_b_D1 where id = 1
+select b2k as blgn, b2r as sortie  from BNEXT_b_D1 where id = 1
 union all
-select id_17 as blgn, nb_17 as sortie   from BNEXT_b_D1 where id = 1
+select b3k as blgn, b3r as sortie   from BNEXT_b_D1 where id = 1
 union all
-select id_28 as blgn, nb_28 as sortie   from BNEXT_b_D1 where id = 1
+select b4k as blgn, b4r as sortie   from BNEXT_b_D1 where id = 1
 union all
-select id_29 as blgn, nb_29 as sortie   from BNEXT_b_D1 where id = 1
+select b5k as blgn, b5r as sortie   from BNEXT_b_D1 where id = 1
 ) as tbleft
 left join
 (
@@ -33,15 +33,15 @@ tbright.z1 = tbleft.blgn
 select tbleft.z1 as B,count(tbright.blgn) as T, MAX (tbright.sortie) as Smax from (select z1 from Bnrz where id <= 49) as tbleft
 left join
 (
-select id_6 as blgn, nb_6 as sortie  from BNEXT_b_D1 where id = 1
+select b1k as blgn, b1r as sortie  from BNEXT_b_D1 where id = 1
 union all
-select id_9 as blgn, nb_9 as sortie  from BNEXT_b_D1 where id = 1
+select b2k as blgn, b2r as sortie  from BNEXT_b_D1 where id = 1
 union all
-select id_17 as blgn, nb_17 as sortie   from BNEXT_b_D1 where id = 1
+select b3k as blgn, b3r as sortie   from BNEXT_b_D1 where id = 1
 union all
-select id_28 as blgn, nb_28 as sortie   from BNEXT_b_D1 where id = 1
+select b4k as blgn, b4r as sortie   from BNEXT_b_D1 where id = 1
 union all
-select id_29 as blgn, nb_29 as sortie   from BNEXT_b_D1 where id = 1
+select b5k as blgn, b5r as sortie   from BNEXT_b_D1 where id = 1
 ) as tbright
 on
 (
@@ -79,7 +79,26 @@ bool MainWindow::NEW_AnalyserCeTirage(int idTirage,  QString stTblRef,int zone, 
 
     msg1 =  "create table "+nomTable+" (id Integer primary key,";
 
+#ifdef NUM_IN_COLNAME
     msg1 = msg1 + NEW_ColHeaderName(idTirage,zone,pConf);
+#else
+
+    int nb_elem = pConf->nbElmZone[zone];
+    for(int bpos=1;bpos<= nb_elem;bpos++)
+    {
+        msg1 = msg1 +
+                "b" + QString::number(bpos)+"v int, " +
+                "b" + QString::number(bpos)+"k int, " +
+                "b" + QString::number(bpos)+"r int";
+
+        if(bpos< nb_elem)
+        {
+          msg1 = msg1 + ",";
+        }
+
+    }
+
+#endif
 
     msg1 = msg1 + ");";
 #ifndef QT_NO_DEBUG
@@ -246,17 +265,18 @@ bool MainWindow::NEW_SyntheseDeBoule(int uneBoule, int colId, int loop, QString 
 
                 boule++;
 
+#ifdef NUM_IN_COLNAME
+
                 msg1 = "update "+ nomTable+" set id_"+QString::number(uneBoule)+"="
                         +QString::number(id)
                         + ", nb_"+QString::number(uneBoule)+"="
                         +QString::number(nb)+
                         " where "+ nomTable+".id="+QString::number(boule)+";";
-
-#if 0
-                msg1 = "update "+ nomTable+" set r"+QString::number(colId)+"_id="
-                        +QString::number(id)
-                        + ", r"+QString::number(colId)+"_nb="
-                        +QString::number(nb)+
+#else
+                msg1 = "update "+ nomTable+
+                        " set b"+QString::number(colId)+"v=" + QString::number(uneBoule)
+                        + ", b"+QString::number(colId)+"k="  +QString::number(id)
+                        + ", b"+QString::number(colId)+"r="  +QString::number(nb)+
                         " where "+ nomTable+".id="+QString::number(boule)+";";
 #endif
 
